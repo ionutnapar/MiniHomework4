7731c9ad20f4a1207627e31456010d2b0de3a17e
ionutnapar <ionut.napar@gmail.com>
2023-04-05 21:25:35 +0100

    Added a third commit
    deleted some comments and added a new System.out.println

--- src/minihomework4/MiniHomework4.java
+++ src/minihomework4/MiniHomework4.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package minihomework4;
 
 /**
@@ -10,12 +6,10 @@ package minihomework4;
  */
 public class MiniHomework4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is my second commit");
+        System.out.println("This is my third commity");
     }
     
 }

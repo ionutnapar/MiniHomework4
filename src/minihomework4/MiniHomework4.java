0e4fbb0527103d0bb82c3196c32dd2e60ec59f46
ionutnapar <ionut.napar@gmail.com>
2023-04-05 21:24:06 +0100

    Added a second commit

--- src/minihomework4/MiniHomework4.java
+++ src/minihomework4/MiniHomework4.java
@@ -15,6 +15,7 @@ public class MiniHomework4 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is my second commit");
     }
     
 }
